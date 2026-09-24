22e0abda1a8a39b6f1c41f18541d6bf584a3cab5
dhruv16-byte <dhruvsmita16@gmail.com>
2026-09-24 21:08:18 +0530

    restaurant system

--- Practice/restaurant_system.py
+++ Practice/restaurant_system.py
@@ -9,3 +9,29 @@ class Menuitem:
 
 class Restaurant:
     
+    def __init__(self,restuarant_name):
+        self.restuarant_name=restuarant_name
+        self.menu=[]
+        self.orders=[]
+        
+
+    def add_items(self,obj_item):
+        self.menu.append(obj_item)
+
+    def display_menu(self):
+        for obj_list in self.menu :
+            print(f"Name : {obj_list.name}\nPrice : {obj_list.price}\nCategory : {obj_list.category}")
+
+    def place_order(self,obj_menuitem): 
+        self.orders.append(obj_menuitem)
+
+    def show_orders(self):
+        for obj_list in self.orders :
+            print(f"Name : {obj_list.name}\nPrice : {obj_list.price}\nCategory : {obj_list.category}")
+
+    def calculate_bill(self):
+        sum=0
+        for i in self.orders:
+            sum+=i.price
+        print(f"the total is {sum}")
+
